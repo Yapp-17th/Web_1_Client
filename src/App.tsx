021a5436b8bf3b6d92a88d36b1f14243a8eb2734
이규종 <kyujong93@naver.com>
2020-10-30 10:25:11 +0900

    typescript 절대경로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import theme from './style/theme';
-import GlobalStyles from './style/globalStyles';
+import theme from '@style/theme';
+import GlobalStyles from '@style/globalStyles';
 import styled from 'styled-components';
 
 const Text = styled.span`
-    color: ${(props) => props.theme.colors.text};
+  color: ${(props) => props.theme.colors.text};
 `;
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyles />
-            <Text>파스텔로 마음을 전해요</Text>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Text>파스텔로 마음을 전해요</Text>
+    </ThemeProvider>
+  );
 }
 
 export default App;
