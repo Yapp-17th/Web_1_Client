b02eed67f98d297fe7a57f0137beea3f1cd82f7b
JinleeJeong <pjjr0118@gmail.com>
2020-11-07 17:09:36 +0900

    refactor for overlap

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import CakeDetail from 'pages/CakeDetail';
-
+import CakeSearch from 'pages/CakeSearch';
 function Routes() {
   return (
     <Switch>
+      <Route path="/store/cake/search">
+        <CakeSearch />
+      </Route>
       <Route path="/store/cake/:cakeId">
         <CakeDetail />
       </Route>
+      
+
     </Switch>
   );
 }
