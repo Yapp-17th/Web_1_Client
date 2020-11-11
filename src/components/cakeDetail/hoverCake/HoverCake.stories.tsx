855901bb293526301430a6f35053f335db7b4459
이규종 <kyujong93@naver.com>
2020-11-12 00:05:40 +0900

    HoverCake 퍼블리싱

---
+++ src/components/cakeDetail/hoverCake/HoverCake.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+import HoverCake, { HoverCakeProps } from './HoverCake';
+import { ThemeProvider } from 'styled-components';
+import theme from 'style/theme';
+
+export default {
+  title: 'CakeDetail/HoverCake',
+  component: HoverCake,
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<HoverCakeProps> = (args) => <HoverCake {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: '체리 메이 블라썸 케이크',
+  address: '서울 마포구 매봉산로 75',
+};
--- src/pages/CakeDetail/CakeDetail.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function CakeDetail() {
-  return <div>cakeDetail</div>;
-}
-
-export default CakeDetail;
